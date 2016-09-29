3a2406fcd6934764b3ff1b2430695231320b9227
Luis Victoria <luisvictoriaperez@gmail.com>
2016-09-29 00:56:59 -0500

    Add rubocop configuration files

--- test_helper.rb
+++ test_helper.rb
@@ -1,2 +1,7 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
+
+SimpleCov.start do
+  add_filter "/vendor/" # Ignores any file containing "/vendor/" in its path.
+  add_filter "/lib/myfile.rb" # Ignores a specific file.
+end
